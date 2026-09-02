13fb69aedc71870167123611e4b31445e8e6ab85
swathikalyanasundaram <akswathikalyan@gmail.com>
2026-09-03 00:57:00 +0530

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import HeroSection from "./component/HeroSection";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function App() {
+  return <HeroSection />;
+}
+
+export default App;
