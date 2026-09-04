89bb43b0ac0dfc4645ee9a7e1a7010a545354db3
swathikalyanasundaram <167862849+swathikalyanasundaram@users.noreply.github.com>
2026-09-04 20:33:23 +0530

    Remove unused import from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path' // You can delete this line or leave it
 
 export default defineConfig({
   plugins: [react()],
